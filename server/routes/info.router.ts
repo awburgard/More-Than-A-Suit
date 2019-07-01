61e331c4cba61f3a86ff2fb995aef5c9614f807a
Austin Burgard <awburgard@gmail.com>
2019-07-01 10:35:56 -0500

    post works for info

--- server/routes/info.router.ts
+++ server/routes/info.router.ts
@@ -4,17 +4,16 @@ import pool from '../modules/pool';
 import { QueryResult } from "pg";
 
 const router: express.Router = express.Router();
-console.log('WERE IN THE POST!')
 
 router.post('/', (req: Request, res: Response, next: express.NextFunction): void => {
     const queryString: string = `INSERT INTO "user"
                         ("first_name", "last_name", "zip", "phone", "email")
                         VALUES ($1, $2, $3, $4, $5);`;
     pool.query(queryString, [req.body.first_name, req.body.last_name, req.body.zip, req.body.phone, req.body.email])
-        .then((response: any): void => {
+        .then((response: QueryResult): void => {
             res.sendStatus(201);
         })
-        .catch((err: any): void => {
+        .catch((err: QueryResult): void => {
             console.log(`Error posting to user: ${err}`);
             res.sendStatus(500);
         })
