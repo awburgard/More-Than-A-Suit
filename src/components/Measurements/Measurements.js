23680460ae806ae89f4f10dc6d4c7e02448b86d6
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-07-05 09:25:15 -0500

    setting up

--- src/components/Measurements/Measurements.js
+++ src/components/Measurements/Measurements.js
@@ -10,10 +10,53 @@ class Measurements extends Component {
         waist: ''
     }
 
+    onFormChange = (dataname) => event => {
+        this.setState({
+            newApproved: {
+                ...this.state.newApproved,
+                [dataname] : event.target.value
+            }
+        });
+    }
+
+    handleClick = event => {
+        event.preventDefault();
+        this.props.dispatch({
+            type: 'ADD_APPROVED',
+            payload: this.state.newApproved
+        })
+        this.setState({
+            newApproved: {
+                ...this.state.newApproved,
+                name: '',
+                number: '',
+            }
+        });
+    }
+
     render() {
+        const measurementInfo = (
+            <form className="addField" onSubmit={this.addNewApproved}>
+                <span className="formHeader">Add New Approved:</span>
+                <input type="text"
+                    value={this.state.newApproved.name}
+                    onChange={this.onFormChange('name')}
+                    placeholder="Name"
+                />
+                <NumberFormat
+                    format="(###) ###-####"
+                    value={this.state.newApproved.number}
+                    onChange={this.onFormChange('number')}
+                    mask="_"
+                    placeholder="Number"
+                />
+                <input type="submit" value="Add"/>
+            </form>
+    )
+
         return(
             <div>
-                I WOKE UP IN BEAST MODE
+                {measurementInfo}
             </div>
         )
     }
