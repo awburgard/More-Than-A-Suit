4c9c0a0975a8e3f9e7567e4a55ba96ded2768c13
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-07-09 13:01:05 -0500

    stopping point

--- src/components/Measurements/Measurements.js
+++ src/components/Measurements/Measurements.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/mapRedux/mapStateToProps';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 class Measurements extends Component {
     state = {
@@ -47,29 +49,46 @@ class Measurements extends Component {
             <form onSubmit={this.handleClick}>
                 <h1>Measurements</h1><br />
                 <h3>Knowing these measurements is an important part of getting the right fit</h3>
-                <h5>Height</h5>
-                <input type="number"
+                <TextField
+                    label="Height"
+                    helperText="Feet"
                     value={this.state.stats.height_feet}
                     onChange={this.onFormChange('height_feet')}
-                    placeholder="Feet"
+                    endAdornment={<InputAdornment position="end">ft</InputAdornment>}
+                    margin="normal"
+                    aria-describedby="weight-helper-text"
+                    inputProps={{
+                        'aria-label': 'Weight',
+                    }}
                 />
-                <input type="number"
+                <TextField
+                    label=" "
+                    helperText="Inches"
                     value={this.state.stats.height_inches}
                     onChange={this.onFormChange('height_inches')}
-                    placeholder="Inches"
-                />
-                <h5>Weight</h5>
-                <input type="number"
+                    endAdornment={<InputAdornment position="end">ft</InputAdornment>}
+                    margin="normal"
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">in</InputAdornment>,
+                      }}
+                /> <br />
+                <TextField
+                    required={true}
+                    id="standard-required"
+                    label="Weight"
                     value={this.state.stats.weight}
                     onChange={this.onFormChange('weight')}
-                    placeholder="Pounds"
+                    margin="normal"
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">lbs</InputAdornment>,
+                      }}
                 />
                 <h5>Waist</h5>
                 <input type="number"
                     value={this.state.stats.waist}
                     onChange={this.onFormChange('waist')}
                     placeholder="Inches"
-                />
+                /> */}
                 <button type="submit">Next</button>
             </form>
         )
