28ce81d7837781e8b64f866ac91a866b2570f4a9
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-07-10 16:27:26 -0500

    commit updated route api

--- server/modules/googlecalendar.ts
+++ server/modules/googlecalendar.ts
@@ -2,6 +2,9 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 
+const apiKey = 'AIzaSyBHz8gbi56Xuvbd9GPaPC9OUDm9hnLNGeY';
+
+
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // The file token.json stores the user's access and refresh tokens, and is
