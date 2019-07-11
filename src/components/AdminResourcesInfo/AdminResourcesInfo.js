b0baef133242ea06020acb8f96a4345d8ed82b7e
Austin Burgard <awburgard@gmail.com>
2019-07-11 11:49:35 -0500

    adds table to resources

--- src/components/AdminResourcesInfo/AdminResourcesInfo.js
+++ src/components/AdminResourcesInfo/AdminResourcesInfo.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/mapRedux/mapStateToProps';
+import { Paper, Table, TableHead, TableCell, TableRow, TableBody } from '@material-ui/core';
 
 
 class AdminResourcesInfo extends Component {
     render() {
         return (
-            <div>
-                Title: {this.props.resources.title} <br />
-                Description: {this.props.resources.description} <br />
-                Link: <a href={this.props.resources.link}>Link</a> <br />
-                Category: {this.props.resources.category_name}
-            </div>
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left" component="th" scope="row">Title</TableCell>
+                            <TableCell align="left" component="th" scope="row">Description</TableCell>
+                            <TableCell align="left" component="th" scope="row">Link</TableCell>
+                            <TableCell align="left" component="th" scope="row">Category</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableCell>{this.props.resources.title}</TableCell>
+                        <TableCell>{this.props.resources.description}</TableCell>
+                        <TableCell>{this.props.resources.link}</TableCell>
+                        <TableCell>{this.props.resources.category_name}</TableCell>
+                    </TableBody>
+                </Table>
+            </Paper>
         )
     }
 }
