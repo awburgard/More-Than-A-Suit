4e1933eb08c36430551f3250a679493ecf35d1d4
Devon Mitchell <devonamitchell10@gmail.com>
2019-07-12 20:21:46 -0500

    updated the setReview reducer to return appointment time in correct format

--- src/redux/reducers/setReview.reducer.js
+++ src/redux/reducers/setReview.reducer.js
@@ -1,7 +1,13 @@
+import moment from "moment";
+
 const setReview = (state = {}, action) => {
+  let time;
+  if (action.payload && action.payload.appointment_time) {
+    time = moment(action.payload.appointment_time).format('h:mm a')
+  }
     switch (action.type) {
       case 'SET_REVIEW':
-        return {...state, ...action.payload};
+        return {...state, ...action.payload, appointment_time: time};
       default:
         return state;
     }
