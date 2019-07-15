9efb59843da5d53d0d40daa9f09fcf247ca9b362
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-07-15 10:20:06 -0500

    revert themes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { AppBar, Toolbar, Typography, useScrollTrigger, Grid } from '@material-ui/core';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/mapRedux/mapStateToProps';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -24,6 +25,7 @@ ElevationScroll.propTypes = {
 
 function Nav(props) {
   return (
+    <MuiThemeProvider theme={props.theme}>
       <React.Fragment>
         <ElevationScroll {...props}>
           <AppBar >
@@ -38,6 +40,7 @@ function Nav(props) {
         </ElevationScroll>
         <Toolbar />
       </React.Fragment>
+    </MuiThemeProvider>
   );
 }
 export default connect(mapStateToProps)(Nav);
