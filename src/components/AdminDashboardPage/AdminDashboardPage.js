3a69eb967573653b1a57b4a3c23dbcce7847aba8
Austin Burgard <awburgard@gmail.com>
2019-07-15 14:10:33 -0500

    adds export button to admin dash

--- src/components/AdminDashboardPage/AdminDashboardPage.js
+++ src/components/AdminDashboardPage/AdminDashboardPage.js
@@ -4,7 +4,8 @@ import mapStateToProps from '../../redux/mapRedux/mapStateToProps';
 import AdminResources from '../AdminResources/AdminResources';
 import AdminAppointment from '../AdminAppointment/AdminAppointment';
 import moment from 'moment';
-import { Typography, Box } from '@material-ui/core'
+import { Typography, Box, Grid, Button } from '@material-ui/core';
+import SaveAlt from '@material-ui/icons/SaveAlt';
 
 class AdminDashboardPage extends Component {
   render() {
@@ -20,12 +21,21 @@ class AdminDashboardPage extends Component {
       }
     }
     return (
-          <Box p={2}>
-            <Typography variant="h5">Dashboard</Typography>
-            <AdminAppointment title="Appointments" appointments={appointments} />
-            <AdminResources />
-            <AdminAppointment title="Archived Appointments" appointments={archivedAppointments} archived={true} />
-          </Box>
+      <Box p={2}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={8}>
+            <Typography variant="h4">Admin Dashboard</Typography>
+          </Grid>
+          <Grid item xs={4} >
+            <Typography component="div" align="right">
+            <Button color="primary" variant="outlined"><SaveAlt className="space-right"></SaveAlt>Export</Button>
+            </Typography>
+          </Grid>
+        </Grid>
+        <AdminAppointment title="Appointments" appointments={appointments} />
+        <AdminResources />
+        <AdminAppointment title="Archived Appointments" appointments={archivedAppointments} archived={true} />
+      </Box>
     );
   }
 }
