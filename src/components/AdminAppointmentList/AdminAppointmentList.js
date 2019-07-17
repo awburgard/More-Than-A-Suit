411ca75d54b0d83d6985a7e1c9c043eae48135bf
Austin Burgard <awburgard@gmail.com>
2019-07-17 15:06:25 -0500

    adds mobile styling to appointments

--- src/components/AdminAppointmentList/AdminAppointmentList.js
+++ src/components/AdminAppointmentList/AdminAppointmentList.js
@@ -2,40 +2,50 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/mapRedux/mapStateToProps';
 import AdminAppointmentItem from '../AdminAppointmentItem/AdminAppointmentItem';
-import {Paper, Table, TableHead, TableRow, TableCell, TableBody} from '@material-ui/core'
+import { Grid, Paper, Typography } from '@material-ui/core'
 
 class AdminAppointmentList extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.dispatch({
       type: 'GET_ADMIN_APPOINTMENT'
     })
   }
   render() {
-    const appointmentElement = this.props.appointments.map((appointment, index)=>{
-      return <AdminAppointmentItem appointment={appointment} index={index} key={index} archived={this.props.archived}/>
+    const appointmentElement = this.props.appointments.map((appointment, index) => {
+      return <AdminAppointmentItem appointment={appointment} index={index} key={index} archived={this.props.archived} />
     })
     return (
-         <Paper elevation={5}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="left" component="th" scope="row">Name</TableCell>
-                <TableCell align="left" component="th" scope="row">Phone Number</TableCell>
-                <TableCell align="left" component="th" scope="row">Zip Code</TableCell>
-                <TableCell align="left" component="th" scope="row">Height</TableCell>
-                <TableCell align="left" component="th" scope="row">Weight</TableCell>
-                <TableCell align="left" component="th" scope="row">Waist</TableCell>
-                <TableCell align="left" component="th" scope="row">Need</TableCell>
-                <TableCell align="left" component="th" scope="row">Appointment Type</TableCell>
-                <TableCell align="left" component="th" scope="row">Appointment Date</TableCell>
-                <TableCell align="left" component="th" scope="row">Appointment Time</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-         {appointmentElement}
-         </TableBody>
-         </Table>
-         </Paper>
+      <Paper className="gridTable" elevation={5}>
+        <div className="gridTable-row gridTable-row_smHide">
+          <Grid container spacing={2}>
+            <Grid item md={2}>
+              <Typography>Name</Typography>
+            </Grid>
+            <Grid item md={2}>
+              <Typography>Phone Number</Typography>
+            </Grid>
+            <Grid item md={1}>
+              <Typography>Zip Code</Typography>
+            </Grid>
+            <Grid item md={1}>
+              <Typography>Height</Typography>
+            </Grid>
+            <Grid item md={1}>
+              <Typography>Weight</Typography>
+            </Grid>
+            <Grid item md={1}>
+              <Typography>Waist</Typography>
+            </Grid>
+            <Grid item md={1}>
+              <Typography>Need</Typography>
+            </Grid>
+            <Grid item md={2}>
+              <Typography>Appointment</Typography>
+            </Grid>
+          </Grid>
+        </div>
+        {appointmentElement}
+      </Paper>
     );
   }
 }
