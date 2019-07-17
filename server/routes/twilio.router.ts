142a66ec6c856cfce3c029bd3cd61fecde6e5eee
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-07-17 11:31:58 -0500

    changed text message text to mention SuitsforHire

--- server/routes/twilio.router.ts
+++ server/routes/twilio.router.ts
@@ -7,7 +7,7 @@ const router: express.Router = express.Router();
 
 router.post('/confirm/text', (req: Request, res: Response, next: express.NextFunction): void => {
     textGentleman(req.body.phoneNumber,
-        `Thank you! Your SuitsforHire appointment is at ${moment(req.body.appointmentTime, 'HH:mm:ss').format(`h:mm a`)} on ${moment(req.body.appointmentDate).format(`MMMM Do YYYY`)}`
+        `Thank you! Your SuitsforHire appointment is at ${moment(req.body.appointmentTime, 'HH:mm:ss').format(`h:mm a`)} on ${moment(req.body.appointmentDate).format(`MMMM Do YYYY`)}.`
     );
     res.send(200);
 });
